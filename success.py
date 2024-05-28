a511bf1b6aee66276f9f996a6bf5cce8148416ef
Gopu k <92145286+Gopuk@users.noreply.github.com>
2024-05-28 19:28:18 +0530

    Added power operation

--- success.py
+++ success.py
@@ -1,10 +1,11 @@
 
 display='''
-1.addition of two numbers
-2.subraction of two numbers
-3.multiplication of two numbers
-4.division of two numbers
-5.exit the program'''
+1.Addition of two numbers
+2.Subraction of two numbers
+3.Multiplication of two numbers
+4.Division of two numbers
+5.Power Of Number
+6.Exit the program'''
 while True:
       a=int (input("enter value of a : "))
       b=int (input("enter value of b : "))
@@ -22,5 +23,10 @@ while True:
                   print(f"division of two numbers = {a/b}")
             except:
                   print("Zero Division Error")
-      elif ch==5 :
+      elif ch==5:
+            print(f"{a} Power {b} = {a**b}")
+      elif ch==6 :
             exit()
+      else:
+            print("Something Wrong!")
+
